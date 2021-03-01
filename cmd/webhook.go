3f444049f00ed16b6c00e0ca4531437c6fd7c76a
4 Lane <real.four.lane@gmail.com>
2021-03-01 13:49:36 +0530

    initial commit

--- cmd/webhook.go
+++ cmd/webhook.go
@@ -34,8 +34,8 @@ var ignoredNamespaces = []string{
 }
 
 const (
-	admissionWebhookAnnotationInjectKey = "sidecar-injector-webhook.morven.me/inject"
-	admissionWebhookAnnotationStatusKey = "sidecar-injector-webhook.morven.me/status"
+	admissionWebhookAnnotationInjectKey = "tailhook-webhook.4lane.legit/inject"
+	admissionWebhookAnnotationStatusKey = "tailhook-webhook.4lane.legit/status"
 )
 
 type WebhookServer struct {
